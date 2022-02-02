824801f4a3a64ab4fa86d9c5cad75ea63e6f7bcf
Shanron999 <roshan2000@hotmail.fr>
2022-02-02 22:33:05 +0100

    rajouts score dificulté 02022022

--- ProjetcalculeMental/app/src/main/java/com/example/projetcalculemental/Dificultes.java
+++ ProjetcalculeMental/app/src/main/java/com/example/projetcalculemental/Dificultes.java
@@ -1,9 +1,13 @@
 package com.example.projetcalculemental;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -46,6 +50,24 @@ public class Dificultes extends AppCompatActivity {
         });
 
 
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.submit_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if(id==R.id.score_menu_button){
+            Intent intent = new Intent(this,ScoreActivity.class);
+            startActivity(intent);
+
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
 }
