18210f3d5eb1276c54f46db2c3015cd322905b39
Shanron999 <roshan2000@hotmail.fr>
2022-02-02 13:46:29 +0100

    adjuste timer 02022022

--- ProjetcalculeMental/app/src/main/java/com/example/projetcalculemental/Play.java
+++ ProjetcalculeMental/app/src/main/java/com/example/projetcalculemental/Play.java
@@ -13,15 +13,17 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.Locale;
+import java.util.concurrent.TimeUnit;
+
 public class Play extends AppCompatActivity {
-    private static long START_TIME_IN_MILIS=60000; //1minute
+    private static long START_TIME_IN_MILIS= TimeUnit.SECONDS.toMillis(10); //1minute 6000
     private TextView Timer;
     private Button Timerbutton;
     private Button Valid;
     private CountDownTimer countDownTimer;
     private TextView False;
     private  long timeleftmiliesecond=START_TIME_IN_MILIS;
-    private boolean timerrunning;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,11 +50,11 @@ public class Play extends AppCompatActivity {
                 startimer();
             }
         });
-
+        updateTimer();
     }
 
     private void startimer(){
-        countDownTimer = new CountDownTimer(timeleftmiliesecond,1000) {
+        countDownTimer = new CountDownTimer(timeleftmiliesecond,1) {
             @Override
             public void onTick(long l) {
                 timeleftmiliesecond=l;
@@ -74,20 +76,31 @@ public class Play extends AppCompatActivity {
         False.setVisibility(View.GONE);
         Timerbutton.setVisibility(View.GONE);
         Valid.setVisibility(View.VISIBLE);
-        timerrunning=true;
+
     }
 
 
     private void updateTimer(){
         int minutes = (int)timeleftmiliesecond/60000;
         int second = (int)timeleftmiliesecond % 60000 / 1000;
-        int score = second + minutes/60;
+        int miliesecond = (int)timeleftmiliesecond % 60000 %1000;
+        int score = miliesecond+second*1000 + minutes*60000;
         String timelefttext;
 
         timelefttext = " "+minutes;
         timelefttext += ":";
         if (second < 10 ) timelefttext += "0";
         timelefttext += second;
+        timelefttext +=":";
+        if(miliesecond<100)timelefttext +="0";
+        if(miliesecond<10)timelefttext +="0";
+        timelefttext += miliesecond;
+
+        /*String timelefttext = String.format(Locale.ENGLISH,"%02d : %02d"
+                ,TimeUnit.MILLISECONDS.toMinutes(1)
+                ,TimeUnit.MILLISECONDS.toSeconds(1)
+                , TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(1)));*/
+
         Timer.setText(timelefttext);
 
 
